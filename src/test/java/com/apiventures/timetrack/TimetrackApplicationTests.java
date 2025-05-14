0e81a0431c581610b4aa6f1e3d10d7ca6a693fee
Edison Nalluri <edison.nalluri@marriott.com>
2025-05-13 21:34:20 -0400

    Base Code

---
+++ src/test/java/com/apiventures/timetrack/TimetrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apiventures.timetrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimetrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
